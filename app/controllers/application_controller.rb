e0ca3eb784e140a5b61fb3fcf6a9a0496b3b26dd
dunow <dunow33@hotmail.com>
2020-11-26 19:12:05 -0500

    adding more comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
+#need to include the securerandom gem for generating a random session secret
 require 'securerandom'
 
+#main controller class inheriting from Sinatra Base
 class ApplicationController < Sinatra::Base
 
+    #main configurations for setting the public folder, the views folder, enabling sessions, and setting the session secret with the secure random gem
     configure do
         set :public_folder, 'public'
         set :views, 'app/views'
@@ -9,10 +12,12 @@ class ApplicationController < Sinatra::Base
         set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
     end
 
+    #route for the initial homepage
     get '/' do
         erb :index
     end
-    
+   
+    #helper methods help determine who the current user is and if they are logged in
     helpers do
         def logged_in?
           !!current_user
