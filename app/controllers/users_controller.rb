80074a62e9d8ba5633d14264282f78cc98869006
dunow <dunow33@hotmail.com>
2020-12-02 11:28:53 -0500

    removing some binding.pry lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,6 @@ class UsersController < ApplicationController
             @user = User.find_by(username: params[:username])
 
             if @user && @user.authenticate(params[:password])
-                binding.pry
                 session[:user_id] = @user.id
                 redirect '/movies'
             else
